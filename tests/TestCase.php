2e663bbad1c753f2d3f12b293325207c1eefdfd9
Elan Ruusamäe <glen@pld-linux.org>
2020-10-12 17:28:01 +0300

    Move loadFixture as TestCase method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,27 @@
 
 namespace XHGui\Test;
 
+use MongoDate;
+use MongoId;
+use XHGui\Saver\SaverInterface;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * Load a fixture into the database.
+     */
+    protected function loadFixture(SaverInterface $saver, $file)
+    {
+        $data = json_decode(file_get_contents($file), true);
+        foreach ($data as $record) {
+            if (isset($record['meta']['request_time'])) {
+                $time = strtotime($record['meta']['request_time']);
+                $record['meta']['request_time'] = new MongoDate($time);
+            }
+            if (isset($record['_id'])) {
+                $record['_id'] = new MongoId($record['_id']);
+            }
+            $saver->save($record, $record['_id'] ?? null);
+        }
+    }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,21 +17,3 @@ $di['config'] = $config;
 
 // Clean up globals.
 unset($di, $config);
-
-/**
- * Load a fixture into the database.
- */
-function loadFixture(SaverInterface $saver, $file)
-{
-    $data = json_decode(file_get_contents($file), true);
-    foreach ($data as $record) {
-        if (isset($record['meta']['request_time'])) {
-            $time = strtotime($record['meta']['request_time']);
-            $record['meta']['request_time'] = new MongoDate($time);
-        }
-        if (isset($record['_id'])) {
-            $record['_id'] = new MongoId($record['_id']);
-        }
-        $saver->save($record, $record['_id'] ?? null);
-    }
-}
